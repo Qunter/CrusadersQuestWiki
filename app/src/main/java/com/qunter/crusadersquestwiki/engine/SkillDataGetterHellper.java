17f2416c546295dc685ecb66d5969e31ebf53145
Qunter <504505485@qq.com>
2017-12-18 23:37:13 +0800

    2017.12.18

--- app/src/main/java/com/qunter/crusadersquestwiki/engine/SkillDataGetterHellper.java
+++ app/src/main/java/com/qunter/crusadersquestwiki/engine/SkillDataGetterHellper.java
@@ -2,25 +2,31 @@ package com.qunter.crusadersquestwiki.engine;
 
 import android.util.Log;
 
+import com.qunter.crusadersquestwiki.entity.SkillData;
+
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ldk on 17-11-27.
  */
 
 public class SkillDataGetterHellper implements DataGetter {
+    List<SkillData> datas = new ArrayList<SkillData>();
     /**
-     * 使用jsoup获取数据
+     * 浣跨敤jsoup鑾峰彇鏁版嵁
      */
     private void getDataFromUrlAndSave(String endString){
         String url = "http://wiki.joyme.com/cq/"+endString;
         Connection conn = Jsoup.connect(url);
-        // 修改http包中的header,伪装成浏览器进行抓取
+        // 淇敼http鍖呬腑鐨刪eader,浼鎴愭祻瑙堝櫒杩涜鎶撳彇
         conn.header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:32.0) Gecko/    20100101 Firefox/32.0");
         Document doc = null;
         try {
@@ -33,6 +39,8 @@ public class SkillDataGetterHellper implements DataGetter {
         }else{
             Elements filterBase = doc.select("div[class=cq_frame]");
             Elements filterfinal = filterBase.select("tr");
+            Elements filterfinall = filterfinal.select("td");
+            /*
             String xml = filterfinal.get(1).toString();
 
             if(xml.length() > 4000) {
@@ -46,31 +54,24 @@ public class SkillDataGetterHellper implements DataGetter {
                 Log.i("getDataFromUrl",xml);
 
             //Log.e("hehe", filterPicUrl.toString() );
-            /*
-            HeroData data;
-            for(Element element:filterMostly){
-                data = new HeroData();
-                data.setHeroDetailUrl(element.absUrl("href"));
-                Log.e("hehe", element.toString() );
-                data.setHeroName(element.attr("title").substring(3));
+            */
+            SkillData data;
+            for(int i=0;i<filterfinall.size()/4;i++){
+                data = new SkillData();
+                data.setSkilllDetailUrl(filterfinall.get(i*4).select("a").get(0).absUrl("href"));
+                Log.e("hehe", filterfinall.get(i*4).toString() );
+                Log.e("heheda",filterfinall.get(i*4).select("a").get(0).absUrl("href"));
+                data.setSkillName(filterfinall.get(i*4).attr("title"));
                 //Log.e("element",element.attr("title").substring(3));
-                data.setHeroPicUrl(element.attr("src"));
-
+                data.setSkillImgUrl(filterfinall.get(i*4).attr("src"));
+                //data.setSkillType(element.select("td").get(2).text());
+                //data.setSkillDetail(element.select("td").get(3).text());
                 //Log.e("url", "do");
                 datas.add(data);
             }
-            for(int i=0;i<filterRate.size()/5;i++){
-                datas.get(i).setHeroPicUrl(filterPicUrl.get(i).attr("src"));
-                //Log.e("url", filterPicUrl.get(i).attr("src"));
-                for(int j=0;j<5;j++){
-                    datas.get(i).setHeroRate(j,Integer.parseInt(filterRate.get(i*5+j).attr("alt").substring(10,11)));
-                    //Log.e("element",filterRate.get(i*5+j).attr("alt").substring(10,11));
-                }
-
-            }
             //getHeroInfoAndSave(filterMostly.toString()+filterRate.toString());
             //Log.e("getDataFromUrl2", filterRate.toString());
-            */
+
         }
     }
 
